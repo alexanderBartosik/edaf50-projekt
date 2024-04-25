7ea1cc12cdd089d4a21eaa63050ff8b8c0b25eef
Alexander Bartosik <alex.bartosik@gmail.com>
2024-04-25 11:38:09 +0200

    CommandHandler, otestad

---
+++ clientserver-main/src/CommandHandler.cc
@@ -0,0 +1,58 @@
+#include "CommandHandler.h"
+
+
+//antar just nu att denna används bara!
+CommandHandler::CommandHandler(shared_ptr<Connection>& c): conn(c) {}
+
+//myclient.cc tar Connection&, idk?
+CommandHandler::CommandHandler(Connection& c) conn(c) {}
+
+
+
+void CommandHandler::send_int(int val) {
+    send_command(Protocol::PAR_NUM);
+
+    conn->write((val >> 24) & 0xFF);
+    conn->write((val >> 16) & 0xFF);
+    conn->write((val >> 8) & 0xFF);
+    conn->write(val & 0xFF);
+}
+
+void CommandHandler::send_string(string val){
+    send_command(Protocol::PAR_STRING);
+    int length = static_cast<int>(val.length());
+    send_int(length);
+
+    for (char c : val) {
+        conn->write(c);
+    }
+
+}
+
+void CommandHandler::send_command(Protocol code) {
+    conn->write(static_cast<unsigned char>(code));
+}
+
+
+int CommandHandler::receive_int() {
+    unsigned char byte1 = conn->read();
+    unsigned char byte2 = conn->read();
+    unsigned char byte3 = conn->read();
+    unsigned char byte4 = conn->read();
+    return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+}
+
+//om byten innan var PAR_STRING, kalla på denna
+string CommandHandler::receive_string() {
+        string s;
+        char   ch;
+        while ((ch = conn->read()) != '$') {
+                s += ch;
+        }
+        return s;
+}
+    
+Protocol CommandHandler::receive_command() {
+    return static_cast<Protocol>(conn->read());
+}
+
---
+++ clientserver-main/src/CommandHandler.h
@@ -0,0 +1,44 @@
+#ifndef COMMANDHANDLER_H
+#define COMMANDHANDLER_H
+
+#include "../include/protocol.h"
+#include "../include/connection.h"
+#include "<memory>"
+
+using std::shared_ptr;
+using std::string;
+
+
+/*
+MESSAGES:
+commandbyte parameters endbyte
+- commandbyte:  e.g. COM_LIST_NG (1 byte)
+- parameters:   starts with either PAR_NUM or PAR_STRING (1 byte). Then either N (4 bytes, significant byte first, MSB) 
+                or N (same, representing nr of chars) and char1 ... charN (N bytes), respectively. 
+- endbyte:      COM_END or ANS_END (1 byte)
+
+example: 
+ANS_LIST_NG 2 13 comp.lang.java 15 comp.lang.c++ ANS_END (2: nr of NGs, 13 and 15: NG IDs)
+where "2", "13", "comp.lang.java", etc are coded as PAR_NUM 2, PAR_NUM 13, PAR_STRING 14 c o m p . (...)"
+
+*/
+class CommandHandler {
+    public:
+    CommandHandler(shared_ptr<Connection>& conn);
+    CommandHandler(Connection& conn);
+
+    void send_int(int val); //sends PAR_NUM val
+    void send_string(string str); //sends PAR_STRING N str (conn.write char by char)
+    void send_command(Protocol code); 
+
+    int receive_int();
+    string receive_string();
+    Protocol receive_command();
+
+
+    private:
+    shared_ptr<Connection> conn;
+
+};
+
+#endif
