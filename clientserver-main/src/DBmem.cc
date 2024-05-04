287815e3a8ac94e6ee676c057dd97cfc016e5c47
WilliamRittfeldt <78855084+WilliamRittfeldt@users.noreply.github.com>
2024-05-04 13:51:31 +0200

    listNewsGroup fix

--- clientserver-main/src/DBmem.cc
+++ clientserver-main/src/DBmem.cc
@@ -20,8 +20,8 @@ DBmem :: ~DBmem() {
 list<NewsGroup> DBmem :: listNewsGroup() const {
     // List all news groups
     list<NewsGroup> newsGroupList;
-    for (auto it = newsGroups.begin(); it != newsGroups.end(); ++it) {
-        newsGroupList.push_back(it->second);
+    for(auto i : newsGroups){
+        newsGroupList.push_back(i.second);
     }
     return newsGroupList;
 }
